b66a130cb78c57343f79f90704e12b1b2248741c
Szymon Piskorz <45246922+SzymonPiskorz@users.noreply.github.com>
2022-01-19 18:38:05 +0000

    Feature World Checkpoints Finished GO-7 (#3)
    * Task Player Enters Save Area Finished GO-17 (#1)
    
    * Task world checkpoint save is correct GO-15 (#2)
    
    * Working on loading data save by the world checkpoint GO-15
    
    * Finished World Checkpoint all info is correct task GO-15

---
+++ Packages/ie.itcarlow.checkpointsytem/Runtime/CheckpointCollider.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointCollider : MonoBehaviour
+{
+    float checkpointHitTimer = 10.0f;
+    public float currentCheckpointTime = 10.0f;
+    public bool savePossible = true;
+    public CheckpointSystem t_checkpointSystem;
+
+    void Awake()
+    {
+        if (FindObjectOfType<CheckpointSystem>() == null)
+        {
+            t_checkpointSystem = new CheckpointSystem();
+        }
+        else
+        {
+            t_checkpointSystem = FindObjectOfType<CheckpointSystem>();
+        }
+    }
+
+    void Update()
+    {
+        if(!savePossible)
+        {
+            if(currentCheckpointTime > 0.0f)
+            {
+                currentCheckpointTime -= Time.deltaTime;
+            }
+            else
+            {
+                currentCheckpointTime = checkpointHitTimer;
+                savePossible = true;
+            }
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision)
+        {
+            savePossible = false;
+            if (t_checkpointSystem != null)
+            {
+                t_checkpointSystem.SaveDataToFile();
+            }
+            else
+            {
+                t_checkpointSystem = new CheckpointSystem();
+                t_checkpointSystem.Awake();
+                t_checkpointSystem.SaveDataToFile();
+            }
+        }
+    }
+}
